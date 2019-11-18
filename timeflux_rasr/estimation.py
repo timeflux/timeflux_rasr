b272f7f9ca10e763ab6fef97ca537a6b25b150f7
Louis Korczowski <louis.korczowski@gmail.com>
2019-11-18 18:38:25 +0100

    added parameters and attributes for fit

--- timeflux_rasr/estimation.py
+++ timeflux_rasr/estimation.py
@@ -6,15 +6,38 @@ class RASR(BaseEstimator, TransformerMixin):
     Implements this (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6499032/) paper.
     Matlab code from the author here:  https://github.com/s4rify/rASRMatlab
 
+    Parameters
+    ----------
+    estimator : string (default: 'scm')
+        covariance matrix estimator. For regularization consider 'lwf' or 'oas'
+        For a complete list of estimator, see `pyriemann.utils.covariance`
+    metric : string | dict (default: 'riemann')
+        The type of metric used for centroid and distance estimation.
+        see `mean_covariance` for the list of supported metric.
+        the metric could be a dict with two keys, `mean` and `distance` in
+        order to pass different metric for the centroid estimation and the
+        distance estimation. Typical usecase is to pass 'logeuclid' metric for
+        the mean in order to boost the computional speed and 'riemann' for the
+        distance in order to keep the good sensitivity for the classification.
+
     Attributes
     ----------
-    foo : float
-        xxxx
+    mixing_ : array, shape(n_chan, n_chan)
+        Mixing matrix computed from geometric median covariance matrix U such as
+        .. math:: mixing_ = M: M*M = U
+    threshold_ : array, shape(n_chan,)
+        Threshold operator used to find the subspace dimension such as:
+        .. math:: threshold_ = T: X_{clean} = m ( V^T_{clean} M )^+ V^T X
+
     """
 
-    def __init__(self, metric='riemann', n_jobs=1):
+    def __init__(self, estimator = 'scm', metric='riemann', n_jobs=1):
         """Init."""
         # TODO:
+
+        self.estimator = estimator
+        self.metric = metric
+
 
     def partial_fit(self, X, y=None):
         """
