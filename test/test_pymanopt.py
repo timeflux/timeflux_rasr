non_linear_eigh validated but pymanopt returns wrong results

test/test_pymanopt.py
@@ -2,10 +2,14 @@ from utils.pymanopt import nonlinear_eigh
 import numpy as np
 
 def test_nonlinear_eigh_unitary():
-    n = 10
+    n = 5
     X = np.eye(n)
-    Xfilt = nonlinear_eigh(X, int(n-1))
-    # TODO: compare with eigh (should be relativement closed for i
+    Xfilt = nonlinear_eigh(X, n-1)
+    # TODO: assert when it should be permuted eye
+    print(Xfilt)
 
 def test_inv_versus_regression():
     # do the test to see if it fits to nonlinear_eigh
+    X = np.eye
+    np.linalg
+    assert False

utils/pymanopt.py
@@ -263,14 +263,27 @@ def nonlinear_eigh(L, p, alpha=1):
     # Define the cost on the Grassmann manifold
     Gr = Grassmann(n, p)
 
-    # TODO: find a way to double check cost function
-    def cost(X):
+    # defined cost function
+    def cost(X):  # matlab comparison with simulation OK <LK>
         rhoX = np.sum(X ** 2, axis=1)
         return 0.5 * np.trace(np.dot(np.transpose(X), np.dot(L, X))) + \
                (alpha/4) * np.dot(np.transpose(rhoX), np.dot(np.linalg.inv(L), rhoX))
 
+    def egrad(X): # checkgradien validation (1e-15
+        rhoX = np.sum(X ** 2, axis=1)
+        return np.dot(L, X) + alpha * np.dot(np.diag(np.dot(np.linalg.inv(L), rhoX)), X)
+
+    def ehess(X, U):
+        rhoX = np.sum(X ** 2, axis=1)
+        rhoXdot = 2 * np.sum(X*U, axis=1)
+        return np.dot(L, U) + \
+               alpha * np.dot(np.diag(np.dot(np.linalg.inv(L), rhoXdot)), X) + \
+               alpha * np.dot(np.diag(np.dot(np.linalg.inv(L), rhoX)), U)
+
+
+
     # Setup the problem
-    problem = Problem(manifold=Gr, cost=cost)
+    problem = Problem(manifold=Gr, cost=cost, egrad=egrad, ehess=ehess)
 
     # Create a solver object
     solver = TrustRegions()