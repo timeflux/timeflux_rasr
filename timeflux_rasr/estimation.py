corrected threshold computation

timeflux_rasr/estimation.py
@@ -133,6 +133,8 @@ class RASR(BaseEstimator, TransformerMixin):
         #TODO: use joblib to parrallelize this loop (code bottleneck)
         for c in range(Ne):
             dist_params[c, :] = _fit_eeg_distribution(rms_sliding[:, c], **self.args_eeg_distribution)
+
+        #TODO: double check threshold_
         self.threshold_ = np.diag(dist_params[:, 0] + self.rejection_cutoff * dist_params[:, 1]).dot(
             np.transpose(evecs))
 
@@ -179,7 +181,7 @@ class RASR(BaseEstimator, TransformerMixin):
             indx = np.argsort(evals)  # sort in ascending
             evecs = evecs[:, indx]
 
-            keep = (evals[indx] < sum((self.threshold_ * evecs) ** 2)) | \
+            keep = (evals[indx] < sum((self.threshold_.dot(evecs)) ** 2)) | \
                    (np.arange(Ne) < (Ne * (1 - self.max_dimension)))
 
             keep = np.expand_dims(keep, 0)  # for element wise multiplication that follows